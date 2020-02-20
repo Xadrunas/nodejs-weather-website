c56ff224ee92bcd0335e69cc733093ce53db6da4
Ouellet-Fournier <jacquot.ouellet.fournier@cgi.com>
2020-02-20 16:36:16 -0500

    Removed console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -14,7 +12,7 @@ weatherForm.addEventListener('submit', e => {
 
     messageOne.textContent = 'Loading data...';
     messageTwo.textContent = '';
-    
+
     fetch('http://localhost:3000/weather?address=' + location).then(response => {
         response.json().then(data => {
             if (data.error) {
